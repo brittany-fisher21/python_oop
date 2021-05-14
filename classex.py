ea488a8b6aa6599ce25069aaa06298a4d5d6c406
Brittany Fisher <bribreana7@gmail.com>
2021-05-14 16:26:33 -0400

    adding file to repo

---
+++ classex.py
@@ -0,0 +1,37 @@
+class Pet:
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = happiness
+        self.hunger = hunger
+        self.mopiness = mopiness
+
+    def eat_food(self):
+        self.fullness += 30
+
+    def get_love(self):
+        self.happiness += 30
+
+    def be_alive(self, hunger, mopiness):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness
+
+cujo = Pet("Cujo")
+cujo.eat_food()
+print(cujo.fullness)
+
+print(cujo.happiness)
+
+
+
+benji = Pet("Benji")
+benji.get_love()
+print(cujo.fullness)
+
+print(benji.happiness)
+
+
+
+
+       
+    
